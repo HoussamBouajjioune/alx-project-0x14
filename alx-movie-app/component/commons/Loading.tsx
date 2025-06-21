4232e415bcbc58639bb65d809c5008586cae2776
houssam bouajjioune <houssambouajjioune1998@gmail.com>
2025-06-21 20:43:12 +0100

    Refactor layout components: remove old implementations and add new Layout, Header, Footer, and LayoutButton components

--- alx-movie-app/component/commons/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Loading: React.FC = () => (
-    <div style={{ textAlign: 'center', padding: '2rem' }}>
-        <span>Loading...</span>
-    </div>
-);
-
-export default Loading;
