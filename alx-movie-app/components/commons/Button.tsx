1e15437c47f477b79be9e109a1282a140a7ea838
houssam bouajjioune <houssambouajjioune1998@gmail.com>
2025-06-21 20:39:35 +0100

    Refactor components and add ButtonProps interface for improved button functionality

--- alx-movie-app/components/commons/Button.tsx
+++ alx-movie-app/components/commons/Button.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import { ButtonProps } from "@/interfaces";
 
-const Button = () => {
+const Button: React.FC<ButtonProps> = ({ title, action }) => {
   return (
-    <button className="bg-blue-500 text-white px-4 py-2 rounded">
-      Click Me
+    <button onClick={action} className="px-8 py-2 border-2 border-[#E2D609] rounded-full hover:bg-[#E2D609] hover:text-black transition-colors duration-300">
+      {title}
     </button>
-  );
-};
+  )
+}
 
 export default Button;
--- alx-movie-app/components/layouts/Layout.tsx
+++ alx-movie-app/components/layouts/Layout.tsx
@@ -1,19 +1,15 @@
-import React, { ReactNode } from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import { ComponentProps } from "@/interfaces";
+import Header from "./Header";
+import Footer from "./Footer";
 
-interface Props {
-  children: ReactNode;
-}
-
-const Layout = ({ children }: Props) => {
+const Layout: React.FC<ComponentProps> = ({ children }) => {
   return (
     <>
       <Header />
       <main className="min-h-screen">{children}</main>
       <Footer />
     </>
-  );
-};
+  )
+}
 
 export default Layout;
